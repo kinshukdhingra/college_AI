e019fbffb7d9d8f6fc2115cd504bab17a863263b
Amit Mahindra <Amythcode@gmail.com>
2024-12-13 17:44:31 +0530

    intent_classification changes

--- Intent_classification.py
+++ Intent_classification.py
@@ -4,71 +4,93 @@
 from sentence_transformers import SentenceTransformer
 from scipy.spatial.distance import cosine
 
-# load the Fine Tuned Model
-# create a instance of model
-model = SentenceTransformer("models/Fine_Tuned_Intent_Model")
 
-# Example FAQs or intents
-# string representation of the Question
-faqs = ["Tell me about the College.",
-        "What courses are offered?",
-        "What is the admission process?",
-        "What are the facilities available here?",
-        "Who is Head of Department Of SSD?",
-        "How many Departments do you have?",
-        "What are the fees?"
-        ]
+def intent_classifier(query):
+    # load the Fine Tuned Model
+    # create a instance of model
+    model = SentenceTransformer("models/Fine_Tuned_Intent_Model")
 
-# Encode FAQs
-# turning the FAQs into vector ( or numerical form which is also know as embedings)
-faq_embeddings = model.encode(faqs, normalize_embeddings=True)
+    # Example FAQs or intents
+    # string representation of the Question
+    faqs = ["Tell me about the College.",
+            "What courses are offered?",
+            "What is the admission process?",
+            "What are the facilities available here?",
+            "Who is Head of Department Of SSD?",
+            "How many Departments do you have?",
+            "What are the fees?"
+            ]
 
-# Create a mapping of embeddings to FAQ labels
-# creating a dictionary representation of the FAQs : Embedings
-# zip() function combines the FAQs list and FAQ embeding list into pairs.
-faq_to_embedding = {faq: embedding for faq, embedding in zip(faqs, faq_embeddings)}  # one line for loop
+    # Encode FAQs
+    # turning the FAQs into vector ( or numerical form which is also know as embedings)
+    faq_embeddings = model.encode(faqs, normalize_embeddings=True)
 
-# Query from the user
-# User's Question
-query = "how many departments does this college have?"
+    # Create a mapping of embeddings to FAQ labels
+    # creating a dictionary representation of the FAQs : Embedings
+    # zip() function combines the FAQs list and FAQ embeding list into pairs.
+    faq_to_embedding = {faq: embedding for faq, embedding in zip(faqs, faq_embeddings)}  # one line for loop
+
+    # Encode the query
+    # Converting the User Question into vector form (or Numerical form also known as embedings)
+    query_embedding = model.encode(query, normalize_embeddings=True)
+
+    # intializing two variables to find the shortest distance
+    closest_faq = None
+    min_distance = float("inf")
+
+    # loops to find the Closest FAQs to the User Query
+    # comparing all the FAQs and user query Embedings
+    for faq, embedding in faq_to_embedding.items():
+        distance = cosine(query_embedding, embedding)
+        if distance < min_distance:
+            min_distance = distance
+            closest_faq = faq
+
+    print(f"User Query: {query}")
+    # printing the closest FAQ
+    # output shortest distance FAQs
+    #print(f"Closest FAQ: {closest_faq}")
+
+    # FAQs and intents are in dictionary form
+    # Each intent is connected to a query
+    faq_to_intent = {
+        "Tell me about the College.": "COLLEGE_OVERVIEW",
+        "What courses are offered?": "COURSE_QUERY",
+        "What is the admission process?": "ADMISSION_QUERY",
+        "What are the facilities available here?": "CAMPUS_QUERY",
+        "Who is Head of Department Of SSD?": "FACULTY_QUERY",
+        "How many Departments do you have?": "DEPARTMENT_QUERY",
+        "What are the fees?": "FEE_QUERY"
+    }
+
+    # Get the matched intent
+    matched_intent = faq_to_intent[closest_faq]
+    print(f"Matched Intent: {matched_intent}")
 
 
-# Encode the query
-# Converting the User Question into vector form (or Numerical form also known as embedings)
-query_embedding = model.encode(query, normalize_embeddings=True)
+# asking user questions repeatedly to identify the intent
+while True:
+    user_query = input("You: ")
+    if user_query.lower() in ["quit", "back", "thats it", "bye"]:
+        break
+    else:
+        intent_classifier(user_query)
 
-# intializing two variables to find the shortest distance
-closest_faq = None
-min_distance = float("inf")
 
-# loops to find the Closest FAQs to the User Query
-# comparing all the FAQs and user query Embedings
-for faq, embedding in faq_to_embedding.items():
-    distance = cosine(query_embedding, embedding)
-    if distance < min_distance:
-        min_distance = distance
-        closest_faq = faq
 
-print(f"User Query: {query}")
-# printing the closest FAQ
-# output shortest distance FAQs
-print(f"Closest FAQ: {closest_faq}")
 
-# FAQs and intents are in dictionary form
-# Each intent is connected to a query
-faq_to_intent = {
-    "Tell me about the College.": "COLLEGE_OVERVIEW",
-    "What courses are offered?": "COURSE_QUERY",
-    "What is the admission process?": "ADMISSION_QUERY",
-    "What are the facilities available here?": "CAMPUS_QUERY",
-    "Who is Head of Department Of SSD?": "FACULTY_QUERY",
-    "How many Departments do you have?": "DEPARTMENT_QUERY",
-    "What are the fees?": "FEE_QUERY"
-}
 
-# Get the matched intent
-matched_intent = faq_to_intent[closest_faq]
-print(f"Matched Intent: {matched_intent}")
+
+
+
+
+
+
+
+
+
+
+
 
 # ***********this is optional******************
 
